93f5e3aaa27b664ea3975356e0f1dace03085c9d
shaeeg <m.shaeeg@gmail.com>
2025-02-16 19:37:50 +0400

    MaxArrayElement task changed to perfection.

--- src/main/java/az/edu/turing/module1/lesson05/MaxArrayElement.java
+++ src/main/java/az/edu/turing/module1/lesson05/MaxArrayElement.java
@@ -1,9 +1,13 @@
 package az.edu.turing.module1.lesson05;
 import java.util.Scanner;
+
 public class MaxArrayElement {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int [] arr = new int[3];
+        System.out.println("Enter the size of the array");
+        int [] arr = new int[scanner.nextInt()];
+        System.out.println("Enter the elements");
         for(int i = 0; i < arr.length; i++){
             arr[i] = scanner.nextInt();
         }
@@ -13,6 +17,7 @@ public class MaxArrayElement {
                 max = arr[i];
             }
         }
+        System.out.print("The max element is: ");
         System.out.println(max);
     }
 }
